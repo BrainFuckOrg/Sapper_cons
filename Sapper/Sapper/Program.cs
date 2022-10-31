9bfd0ae91a3cc8a0c2c86e494af57d5ceac1e190
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-10-31 18:38:55 +0200

    second commit

--- Sapper/Sapper/Program.cs
+++ Sapper/Sapper/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa");
+Console.WriteLine("Sanya can you make normal commit names?");
